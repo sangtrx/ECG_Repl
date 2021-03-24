ab1685a5ab75f6771896de9252a27a4e37d3fa37
DucLe <le.m.duc86@gmail.com>
2021-03-24 19:24:23 +0000

    Change on some files

--- Datasets/cinc17/setup.sh
+++ Datasets/cinc17/setup.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-
 url=https://archive.physionet.org/challenge/2017/
-
-mkdir data && cd data
 
 curl -s -O $url/training2017.zip
 unzip -q training2017.zip -d .
@@ -12,6 +9,4 @@ unzip -q sample2017.zip -d .
 rm -r sample2017.zip
 curl -s -O $url/REFERENCE-v3.csv 
 
-cd ..
-
-python build_datasets.py
+python build_cinc17_dataset.py
