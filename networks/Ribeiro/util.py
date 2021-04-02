c5e3194ad3beebc601fc9f406aaedc2fca17fdf5
DucLe <le.m.duc86@gmail.com>
2021-04-02 21:05:42 +0000

    Change

--- networks/Ribeiro/util.py
+++ networks/Ribeiro/util.py
@@ -1,5 +1,8 @@
 import os
 import pickle
+import os
+import random
+import time
 
 def load(dirname):
     preproc_f = os.path.join(dirname, "preproc.bin")
@@ -9,7 +12,7 @@ def load(dirname):
 
 def save(preproc, dirname):
     preproc_f = os.path.join(dirname, "preproc.bin")
-    with open(preproc_f, 'w') as fid:
+    with open(preproc_f, 'wb') as fid:
         pickle.dump(preproc, fid)
 
 def make_save_dir(dirname, experiment_name):
