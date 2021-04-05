e2a8f5417129693e97f431621d619aedd8e11c3b
DucLe <le.m.duc86@gmail.com>
2021-04-05 18:09:58 +0000

    Change

--- datasets/cinc20/setup.sh
+++ datasets/cinc20/setup.sh
@@ -1,3 +1,32 @@
 #!/bin/bash
 
-url=https://archive.physionet.org/challenge/2017/
+cd /content/ECG_Repl/datasets/cinc20
+
+# download and unzip 
+kaggle datasets download -d bjoernjostein/georgia-12lead-ecg-challenge-database
+unzip -q georgia-12lead-ecg-challenge-database.zip -d .
+rm -r georgia-12lead-ecg-challenge-database.zip
+
+kaggle datasets download -d bjoernjostein/st-petersburg-incart-12lead-arrhythmia-database
+unzip -q st-petersburg-incart-12lead-arrhythmia-database.zip -d .
+rm -r st-petersburg-incart-12lead-arrhythmia-database.zip
+
+kaggle datasets download -d bjoernjostein/ptbxl-electrocardiography-database
+unzip -q ptbxl-electrocardiography-database.zip -d . 
+rm -r ptbxl-electrocardiography-database.zip 
+
+kaggle datasets download -d bjoernjostein/ptb-diagnostic-ecg-database
+unzip -q ptb-diagnostic-ecg-database.zip -d . 
+rm -r ptb-diagnostic-ecg-database.zip 
+
+kaggle datasets download -d bjoernjostein/china-12lead-ecg-challenge-database
+unzip -q china-12lead-ecg-challenge-database.zip -d . 
+rm -r china-12lead-ecg-challenge-database.zip 
+
+kaggle datasets download -d bjoernjostein/china-physiological-signal-challenge-in-2018
+unzip -q china-physiological-signal-challenge-in-2018.zip -d .
+rm -r china-physiological-signal-challenge-in-2018.zip 
+
+kaggle datasets download -d bjoernjostein/physionet-snomed-mappings
+unzip -q physionet-snomed-mappings.zip -d .
+rm -r physionet-snomed-mappings.zip
