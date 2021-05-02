8fc579e3a5dd6b6575b26f2b0dbe844fa9e93a15
DucLe <le.m.duc86@gmail.com>
2021-05-02 12:06:53 +0000

    Update as of May 2nd, 2021

--- networks/AndrewNg/cinc20/load.py
+++ networks/AndrewNg/cinc20/load.py
@@ -39,3 +39,116 @@ def import_key_data(path):
                 age.append(header_data[13][6:-1])
     return gender, age, labels, ecg_filenames
 
+
+def make_undefined_class(labels, df_unscored):
+    df_labels = pd.DataFrame(labels)
+    for i in range(len(df_unscored.iloc[0:,1])):
+        df_labels.replace(to_replace=str(df_unscored.iloc[i,1]), inplace=True ,value="undefined class", regex=True)
+
+    return df_labels
+
+def onehot_encode(df_labels):
+    one_hot = MultiLabelBinarizer()
+    y=one_hot.fit_transform(df_labels[0].str.split(pat=','))
+    print("The classes we will look at are encoded as SNOMED CT codes:")
+    print(one_hot.classes_)
+    y = np.delete(y, -1, axis=1)
+    print("classes: {}".format(y.shape[1]))
+    return y, one_hot.classes_[0:-1]
+
+def get_labels_for_all_combinations(y):
+    y_all_combinations = LabelEncoder().fit_transform([''.join(str(l)) for l in y])
+    return y_all_combinations
+
+def split_data(labels, y_all_combo):
+    folds = list(StratifiedKFold(n_splits=10, shuffle=True, random_state=42).split(labels,y_all_combo))
+    print("Training split: {}".format(len(folds[0][0])))
+    print("Validation split: {}".format(len(folds[0][1])))
+    return folds
+
+def split_data_opt(labels, y_all_combo):
+    folds = list(StratifiedKFold(n_splits=10, shuffle=True, random_state=42).split(labels,y_all_combo))
+    print("Training split: {}".format(len(folds[0][0])))
+    print("Validation split: {}".format(len(folds[0][1])))
+    return folds
+
+def shuffle_batch_generator_demo(batch_size, gen_x,gen_y, gen_z): 
+    np.random.shuffle(order_array)
+    batch_features = np.zeros((batch_size,5000, 12))
+    batch_labels = np.zeros((batch_size,snomed_classes.shape[0])) #drop undef class
+    batch_demo_data = np.zeros((batch_size,2))
+    while True:
+        for i in range(batch_size):
+
+            batch_features[i] = next(gen_x)
+            batch_labels[i] = next(gen_y)
+            batch_demo_data[i] = next(gen_z)
+
+        X_combined = [batch_features, batch_demo_data]
+        yield X_combined, batch_labels
+        
+def shuffle_batch_generator(batch_size, gen_x,gen_y): 
+    np.random.shuffle(order_array)
+    batch_features = np.zeros((batch_size,5000, 12))
+    batch_labels = np.zeros((batch_size,snomed_classes.shape[0])) #drop undef class
+    while True:
+        for i in range(batch_size):
+
+            batch_features[i] = next(gen_x)
+            batch_labels[i] = next(gen_y)
+            
+        yield batch_features, batch_labels
+
+def generate_y_shuffle(y_train):
+    while True:
+        for i in order_array:
+            y_shuffled = y_train[i]
+            yield y_shuffled
+
+
+def generate_X_shuffle(X_train):
+    while True:
+        for i in order_array:
+                #if filepath.endswith(".mat"):
+                    data, header_data = load_challenge_data(X_train[i])
+                    X_train_new = pad_sequences(data, maxlen=5000, truncating='post',padding="post")
+                    X_train_new = X_train_new.reshape(5000,12)
+                    yield X_train_new
+
+def calculating_class_weights(y_true):
+    number_dim = np.shape(y_true)[1]
+    weights = np.empty([number_dim, 2])
+    for i in range(number_dim):
+        weights[i] = compute_class_weight('balanced', [0.,1.], y_true[:, i])
+    return weights
+
+def generate_validation_data(ecg_filenames, y,test_order_array):
+    y_train_gridsearch=y[test_order_array]
+    ecg_filenames_train_gridsearch=ecg_filenames[test_order_array]
+
+    ecg_train_timeseries=[]
+    for names in ecg_filenames_train_gridsearch:
+        data, header_data = load_challenge_data(names)
+        data = pad_sequences(data, maxlen=5000, truncating='post',padding="post")
+        ecg_train_timeseries.append(data)
+    X_train_gridsearch = np.asarray(ecg_train_timeseries)
+
+    X_train_gridsearch = X_train_gridsearch.reshape(ecg_filenames_train_gridsearch.shape[0],5000,12)
+
+    return X_train_gridsearch, y_train_gridsearch
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
